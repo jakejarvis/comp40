0602c3ac7206179b45d2527c5dfb09655e447772
Jake Jarvis <jakejarvis@gmail.com>
2015-01-23 22:57:51 -0500

    HW1 submitted

--- intro/brightness.c
+++ intro/brightness.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pnmrdr.h>
 
 extern void compute_brightness(FILE *fp);
+extern void check_validity(FILE *fp);
 
 int main(int argc, char *argv[])
 {
-        if(argc == 1) {
+        if(argc == 1) {    /* no filename, use stdin */
                 compute_brightness(stdin);
-        } else {
-                for(int i = 1; i < argc; i++) {
-                        FILE *fp = fopen(argv[i], "r");
+        } else if (argc == 2) {     /* one filename, use it */
+                FILE *fp = fopen(argv[1], "r");
 
-                        if(fp == NULL) {
-                                fprintf(stderr,
-                                        "Could not open file %s for reading\n",
-                                        argv[i]);
-                        } else {
-                                compute_brightness(fp);
-                                fclose(fp);
-                        }
+                if(fp == NULL) {
+                        fprintf(stderr,
+                                "ERROR: Could not open file %s for reading.\n",
+                                argv[1]);
+                } else {
+                        // check_validity(fp); /* NOTE BELOW */
+                        compute_brightness(fp);
+                        fclose(fp);
                 }
+        } else {    /* more than one filename, abort */
+                fprintf(stderr,
+                        "ERROR: Please only give one filename at a time.\n");
         }
         
         return 0;
@@ -30,12 +34,31 @@ extern void compute_brightness(FILE *fp)
         Pnmrdr_T image = Pnmrdr_new(fp);
         Pnmrdr_mapdata map = Pnmrdr_data(image);
         double sum = 0;
-        
+
         for(unsigned int i = 0; i < (map.width * map.height); i++) {
                 sum += Pnmrdr_get(image);
         }
-        
+
         double average = sum / (map.width * map.height * map.denominator);
-        
-        printf("%.3f\n", average);       
+
+        printf("%.3f\n", average);
+
+}
+
+/* NOTE: We tried very hard to implement error handling - aka showing our own
+ * error message if the image isn't in a valid graymap format instead of saying
+ * "Uncaught exception" (if the file is a PNG, for example). However, this code 
+ * seems to cause more errors than it handles. We had to prioritize and leave
+ * this out last minute. Sorry!!!!
+ */
+extern void check_validity(FILE *fp)
+{
+        TRY
+                Pnmrdr_T image = Pnmrdr_new(fp);
+                (void)image; /* compiler makes us use all variables */
+        EXCEPT(Pnmrdr_Badformat)
+                fprintf(stderr,
+                        "ERROR: Data is not a portable grayscale image.\n");
+                exit(EXIT_FAILURE);
+        END_TRY;       
 }
--- intro/fgroups.c
+++ intro/fgroups.c
@@ -35,16 +35,20 @@ int main(int argc, char *argv[])
         char buffer[2048];
 
         while (fgets(buffer, sizeof(buffer), stdin)) {
-                if (buffer[0] == ' ' || buffer[0] == '\t') {  
+                if (strchr(buffer,'\n') == NULL) {
+                        fprintf(stderr, "INPUT: \"%s\"\n%s\n\n", buffer,
+                                        "Line truncated.");
+                        char temp_char;
+                        while ((temp_char = getchar()) != '\n'
+                                && temp_char != EOF);
+                }
+
+                if (buffer[0] == ' ' || buffer[0] == '\t') {
                         /* line starts with whitespace, no good */
                         fprintf(stderr, "BAD INPUT: \"%s\"\n%s\n\n", buffer, 
                                         "Starts with whitespace.");
-                } else if (buffer[0] != '\n') {    /* skip this line if it's 
+                } else if (buffer[0] != '\n'){    /* skip this line if it's
                                                     * just a newline */
-
-                        if(buffer[sizeof(buffer)-1] != '\0') {
-                            printf("truncating name....");
-                        }
 
                         /* invariant: if there's no space or tab on this line,
                          * these will always equal -1 */
@@ -87,65 +91,55 @@ int main(int argc, char *argv[])
                         memcpy(name_untrimmed, &buffer[whitespace_index],
                                name_length);
 
-
                         /* ...but we have to trim the whitespace on both ends
                          * start with leading whitespace: */
-                        int first_index = 0;
-                        while (isspace(name_untrimmed[first_index]))
+                        unsigned first_index = 0;
+                        while (isspace(name_untrimmed[first_index])
+                               && first_index < sizeof(name_untrimmed))
                                 first_index++;
 
                         /* now trim trailing whitespace: */
-                        int last_index = strlen(name_untrimmed);
-                        while (isspace(name_untrimmed[last_index])
+                        unsigned last_index = strlen(name_untrimmed);
+                        while ((isspace(name_untrimmed[last_index])
                            || name_untrimmed[last_index] == '\0')
+                               && last_index > 0)
                                 last_index--;
 
-                        /* replace name with trimmed version & terminate */
-                        name_length = last_index - first_index + 1;
-                        char *name = malloc(512);
-                        memcpy(name, &name_untrimmed[first_index], name_length);
-                        name[name_length] = '\0';
+                        /* check whether the space after fprint is all
+                         * white space */
+                        if (first_index <= last_index) {
+                                /* replace name with trimmed version & 
+                                 * terminate */
+                                name_length = last_index - first_index + 1;
+                                char *name = malloc(512);
+                                memcpy(name, &name_untrimmed[first_index], 
+                                       name_length);
+                                name[name_length] = '\0';
 
+                                /* the fingerprint must be converted into an 
+                                 * atom now because we need to compare them to 
+                                 * each other */
+                                const char *fingerprint_atom = 
+                                                      Atom_string(fingerprint);
 
+                                /* retrieve the existing list of names from the
+                                 * table corresponding to the fingerprint we're
+                                 * looking at for this line. (there might not 
+                                 * be one yet.) */
+                                List_T name_list = Table_get(table,
+                                                             fingerprint_atom);
 
-                        // DEBUG
-                        printf("line: %s\n", buffer);
-                        printf("index of space: %i\n", space_index);
-                        printf("index of tab: %i\n", tab_index);
-                        printf("whitespace index: %i\n", whitespace_index);
-                        printf("first character index: %i\n", first_index);
-                        printf("last character index: %i\n", last_index);
-                        
-                        printf("fingerprint: %s\n", fingerprint);
-                        printf("name: %s\n", name);
-                        
-                        printf("\n\n");
+                                /* push the name to the top of the list we 
+                                 * received and overwrite the old list */
+                                name_list = List_push(name_list, name);
 
+                                /* put the modified list back into the table 
+                                 * under the fingerprint key */
+                                Table_put(table, fingerprint_atom, name_list);
+                        } /* first_index If end */
+                } /* If end */
+        } /* While end*/
 
-
-                        /* the fingerprint must be converted into an atom now
-                         * because we need to compare them to each other */
-                        const char *fingerprint_atom = Atom_string(fingerprint);
-
-                        /* retrieve the existing list of names from the table
-                         * corresponding to the fingerprint we're looking at
-                         * for this line. (there might not be one yet.) */
-                        List_T name_list = Table_get(table, fingerprint_atom);
-
-                        /* push the name to the top of the list we received and
-                         * overwrite the old list */
-                        name_list = List_push(name_list, name);
-
-                        /* put the modified list back into the table under the
-                         * fingerprint key */
-                        Table_put(table, fingerprint_atom, name_list);
-
-
-
-                        FREE(buffer);
-                }
-        }
-        
         if (ferror(stdin))    /* something went wrong with reading stdin */
                 fprintf(stderr, "BAD INPUT: Error reading stdin\n");
 
@@ -154,7 +148,7 @@ int main(int argc, char *argv[])
 
         /* all done with the table, free up memory */
         Table_free(&table);
-
+        
         return 0;
 }
 
@@ -164,10 +158,12 @@ void print_group(const void *key, void **value, void *cl)
         (void)key;
         (void)cl;
 
-        /* if there's only one name to this fingerprint, don't print anything */
+        /* if there's only one name to this fingerprint, don't print 
+         * anything */
         if (List_length(*value) > 1) {
                 List_map(*value, print_name, NULL);
-                printf("\n"); /* print an extra newline at the end of the group */
+                printf("\n"); /* print an extra newline at the end of the 
+                               * group */
         }
 
         /* all done with this pair, free up memory */
